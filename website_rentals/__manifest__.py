7ed88fdc183d986b7eedcb739f604bc57243828e
Ward Marissen <ward.marissen@gmail.com>
2022-02-21 15:03:10 +0100

    Update manifest version

--- website_rentals/__manifest__.py
+++ website_rentals/__manifest__.py
@@ -4,7 +4,7 @@
     "author": "Mainframe Monkey",
     "website": "http://www.mainframemonkey.com",
     "category": "Website",
-    "version": "14.0.1.3",
+    "version": "14.0.2.0",
     "depends": [
         "sale",
         "sale_management",
