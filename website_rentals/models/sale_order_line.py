3e8cedabd7eb4984268d471ab35c26f4747a47ab
Ward M <ward.marissen@gmail.com>
2022-03-07 16:26:54 +0100

    [FIX] website_rentals: Field naming

--- website_rentals/models/sale_order_line.py
+++ website_rentals/models/sale_order_line.py
@@ -5,8 +5,15 @@ import datetime
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
-    pickup_date_no_timezone = fields.Char(string="Pickup", compute='_compute_pickup_date_no_timezone')
-    return_date_no_timezone = fields.Char(string="Return", compute='_compute_pickup_date_no_timezone')
+    pickup_date_no_timezone = fields.Char(
+        string="Pickup date without timezone",
+        compute='_compute_pickup_date_no_timezone'
+    )
+
+    return_date_no_timezone = fields.Char(
+        string="Return date without timezone",
+        compute='_compute_pickup_date_no_timezone'
+    )
 
     def _compute_pickup_date_no_timezone(self):
         lang = self.env.user.lang
@@ -15,6 +22,10 @@ class SaleOrderLine(models.Model):
         lang_id = self.env['res.lang'].search([('code', '=', lang)], limit=1)
         for record in self:
             if record.pickup_date:
-                record.pickup_date_no_timezone = record.pickup_date.replace(tzinfo=None).strftime(lang_id.date_format + ' ' + lang_id.time_format)
+                record.pickup_date_no_timezone = record.pickup_date.replace(tzinfo=None).strftime(
+                    lang_id.date_format + ' ' + lang_id.time_format
+                )
             if record.return_date:
-                record.return_date_no_timezone = record.return_date.replace(tzinfo=None).strftime(lang_id.date_format + ' ' + lang_id.time_format)
+                record.return_date_no_timezone = record.return_date.replace(tzinfo=None).strftime(
+                    lang_id.date_format + ' ' + lang_id.time_format
+                )
